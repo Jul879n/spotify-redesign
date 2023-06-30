aa948288d7dfd57e1871c0462439bd1c98958db0
Jul879n <l.araya@duocuc.cl>
2023-06-30 13:24:07 -0400

    casi

--- assets/librerias/js/mis-scripts.js
+++ assets/librerias/js/mis-scripts.js
@@ -4,16 +4,34 @@ jQuery(document).ready(function ($) {
     $("#principal").removeClass("tema-oscuro tema-amoled");
     $("#reproducir").addClass("tema-blanco");
     $("#principal").addClass("tema-blanco");
-    $(".tema-blanco .tema").find(".text-white").addClass("text-dark").removeClass("text-white");
-      $("#confirmar").removeClass("disabled");
+    $("#menu").find(".bg-white").addClass("bg-dark").removeClass("bg-white");
+    $("#menu")
+      .find(".text-dark")
+      .addClass("text-white")
+      .removeClass("text-dark");
+    $("#menu").addClass("vidrio").removeClass("border-light transparente");
+    $(".tema-blanco .tema")
+      .find(".text-white")
+      .addClass("text-dark")
+      .removeClass("text-white");
+    $("#confirmar").removeClass("disabled");
   });
   $("#tema-oscuro").click(function () {
     $("#reproducir").removeClass("tema-blanco tema-amoled");
     $("#principal").removeClass("tema-blanco tema-amoled");
     $("#reproducir").addClass("tema-oscuro");
     $("#principal").addClass("tema-oscuro");
-    $(".tema-oscuro .tema").find(".text-dark").addClass("text-white").removeClass("text-dark");
-      $("#confirmar").removeClass("disabled");
+    $("#menu").find(".bg-dark").addClass("bg-white").removeClass("bg-dark");
+    $("#menu").addClass("vidrio").removeClass("border-light transparente");
+    $("#menu")
+      .find(".text-white")
+      .addClass("text-dark")
+      .removeClass("text-white");
+    $(".tema-oscuro .tema")
+      .find(".text-dark")
+      .addClass("text-white")
+      .removeClass("text-dark");
+    $("#confirmar").removeClass("disabled");
     $("#confirmar").removeClass("disabled");
   });
   $("#tema-amoled").click(function () {
@@ -21,11 +39,139 @@ jQuery(document).ready(function ($) {
     $("#principal").removeClass("tema-blanco tema-oscuro");
     $("#reproducir").addClass("tema-amoled");
     $("#principal").addClass("tema-amoled");
+    $("#menu").find(".bg-dark").addClass("bg-white").removeClass("bg-dark");
+    $("#menu")
+      .find(".text-white")
+      .addClass("text-dark")
+      .removeClass("text-white");
+    $("#menu").removeClass("vidrio").addClass("border-light transparente");
     $(".tema-amoled").find(".card").addClass("border-light");
-    $(".tema-amoled .tema").find(".text-dark").addClass("text-white").removeClass("text-dark");
+    $(".tema-amoled .tema")
+      .find(".text-dark")
+      .addClass("text-white")
+      .removeClass("text-dark");
     $("#confirmar").removeClass("disabled");
   });
   $("#confirmar").click(function () {
     $("#selector-tema").remove();
+  });
+  // Obtener la canción y configurar la fuente del reproductor de audio
+  var cancion = $("#reproducir .song");
+  var audio = $("#cancion");
+  var source = audio.find("source");
+
+  // Declarar una variable para llevar el registro de la canción actual
+  var cancionActual = 0;
+
+  // Funciones para cargar y reproducir la canción actual
+  function cargarCancion() {
+    var cancionActualElement = $(".song").eq(cancionActual % $(".song").length);
+    var src = cancionActualElement.attr("data-src");
+    var title = cancionActualElement.attr("data-title");
+    source.attr("src", src);
+    cancion.html(title);
+    audio[0].load();
+
+    // Establecer el valor inicial de la duración en "00:00"
+    $("#duracion").html("00:00");
+
+    // Establecer el valor inicial del rango en 0
+    $("#tiempo-reproduccion").val(0);
+
+    // Actualizar la barra de progreso
+    audio[0].addEventListener("loadedmetadata", function () {
+      var duracion = Math.floor(audio[0].duration);
+      $("#duracion").html(formatTime(duracion));
+      $("#tiempo-reproduccion").attr("max", duracion);
+    });
+  }
+
+  function reproducirCancion() {
+    audio[0].play();
+    $(".play")
+      .find(".fa-circle-play")
+      .addClass("fa-pause")
+      .removeClass("fa-circle-play");
+  }
+
+  // Manejar el evento de finalización de la canción actual
+  audio[0].addEventListener("ended", function () {
+    if (cancionActual < $(".song").length - 1) {
+      cancionActual++;
+    } else {
+      cancionActual = 0;
+    }
+    cargarCancion();
+    reproducirCancion();
+  });
+
+  // Manejar el evento de clic en el botón de siguiente
+  $(".siguiente").on("click", function () {
+    if (cancionActual < $(".song").length - 1) {
+      cancionActual++;
+    } else {
+      cancionActual = 0;
+    }
+    cargarCancion();
+    reproducirCancion();
+  });
+
+  // Manejar el evento de clic en el botón de reproducción
+  $(".play").on("click", function () {
+    if (audio[0].paused) {
+      reproducirCancion();
+    } else {
+      audio[0].pause();
+      $(".play")
+        .find(".fa-pause")
+        .addClass("fa-circle-play")
+        .removeClass("fa-pause");
+    }
+  });
+
+  // Actualizar la barra de progreso en función del tiempo de reproducción
+  audio.on("timeupdate", function () {
+    var duracion = audio[0].duration;
+    var tiempoActual = audio[0].currentTime;
+
+    // Actualizar el tiempo actual y la duración
+    $("#reproducir #tiempo-actual").html(formatTime(tiempoActual));
+    $("#reproducir #duracion").html(formatTime(duracion));
+
+    // Actualizar la barra de progreso
+    var porcentaje = (tiempoActual / duracion) * 100;
+    $("#reproducir #tiempo-reproduccion").val(porcentaje);
+  });
+
+  // Manejar el evento de cambio en la barra de progreso
+  $("#reproducir #tiempo-reproduccion").on("input", function () {
+    var porcentaje = $(this).val();
+    var duracion = audio[0].duration;
+
+    // Actualizar la posición de reproducción
+    audio[0].currentTime = (porcentaje / 100) * duracion;
+  });
+
+  // Función de ayuda para formatear el tiempo en formato mm:ss
+  function formatTime(segundos) {
+    var minutos = Math.floor(segundos / 60);
+    var segundosRestantes = Math.floor(segundos % 60);
+
+    return (
+      minutos.toString().padStart(2, "0") +
+      ":" +
+      segundosRestantes.toString().padStart(2, "0")
+    );
+  }
+
+  // Cargar la primera canción
+  cargarCancion();
+
+  //evitar formulario
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+  });
+  $(".nombre-artista").click(function() {
+    $("#artista").removeClass("no-ver");
   });
 });
--- template-parts/content-home-page.php
+++ template-parts/content-home-page.php
@@ -12,25 +12,42 @@
 
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div id="fondo" class="container-fluid">
-	<img id="menu" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="">
+		<div id="menu" class="card rounded-5 vidrio">
+			<div class="card-body d-flex">
+				<button class="ms-2 me-2 btn bg-dark circulo text-white d-flex justify-content-center align-items-center"><i class="fa-solid fa-angle-left"></i></button>
+				<button class="ms-2 me-2 btn bg-dark circulo text-white d-flex justify-content-center align-items-center"><i class="fa-solid fa-angle-right"></i></button>
+				<button  data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" class="ms-2 me-2 btn bg-dark circulo text-white d-flex justify-content-center align-items-center"><i class="fa-solid fa-bars"></i></button>
+			</div>
+			
+		</div>
+
 		<div class="row">
 			<div id="reproducir" class="col-12 col-sm-4">
 				<div class="card tema bg-secondary text-white mt-3 mb-3 ">
 					<div id="info-reproduccion" class="card-body d-flex flex-column align-items-center justify-content-center">
 						<img class="mitad rounded-3" src="" alt="">
+						<div class="song" style="display: none;" data-src="https://laraya.laboratoriodiseno.cl/spotify/wp-content/uploads/2023/05/Tjarnheden-Farsjon.mp3" data-title="Canción 1"></div>
+						<div class="song" style="display: none;" data-src="https://radios.liberaturadio.org/mestiza.ogg" data-title="Canción 2"></div>
+						<audio style="display: none;" id="cancion" controls>
+							<source src="" type="audio/mp3">
+						</audio>
 						<form class="mt-3 mitad" action="">
-							<input class="w-100" type="range" name="tiempo-reproduccion" id="tiempo-reproduccion">
+							<input class="w-100 border-0 progress-bar" type="range" name="tiempo-reproduccion" id="tiempo-reproduccion">
+							<div class="d-flex justify-content-between">
+								<span class="text-white" id="tiempo-actual">00:00</span>
+								<span class="text-white" id="duracion">00:00</span>
+							</div>
 							<div class="d-flex justify-content-around" id="botones-reproducir">
-								<button class="btn text-white"><i class="fa-solid fa-shuffle"></i></button>
-								<button class="btn text-white"><i class="fa-solid fa-backward-step"></i></button>
-								<button class="btn text-white fs-1"><i class="fa-solid fa-circle-play"></i></button>
-								<button class="btn text-white"><i class="fa-solid fa-forward-step"></i></button>
-								<button class="btn text-white"><i class="fa-solid fa-repeat"></i></button>
+								<button class="btn text-white mix"><i class="fa-solid fa-shuffle"></i></button>
+								<button class="btn text-white atras"><i class="fa-solid fa-backward-step"></i></button>
+								<button class="btn text-white fs-1 play"><i class="fa-solid fa-circle-play"></i></button>
+								<button class="btn text-white siguiente"><i class="fa-solid fa-forward-step"></i></button>
+								<button class="btn text-white repetir"><i class="fa-solid fa-repeat"></i></button>
 							</div>
 						</form>
 					</div>
 				</div>
-				<div class="card tema bg-secondary text-white">
+				<div id="artista" class="card tema bg-secondary text-white no-ver">
 					<div class="card-body">
 						<h1 class="text-white">Artista</h1>
 						<p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, beatae officiis iure ratione delectus ad cupiditate nam, quos veritatis accusantium id illo voluptatum eveniet veniam sit consequatur vitae possimus dolor.</p>
@@ -40,22 +57,28 @@
 				</div>
 			</div>
 			<div id="principal" class="col-12 col-sm">
-			
 				<div id="contenido" class="card tema bg-secondary text-white mt-3 mb-3">
 
 					<div class="card-body">
-						<h1 class="text-white">Playlists</h1>
+						<h1 class="text-white nombre-artista">Playlists</h1>
 					</div>
 				</div>
 				<div id="reproductor-2" class="card tema bg-secondary text-white mt-3 mb-3">
 					<div id="info-reproduccion" class="card-body d-flex flex-column align-items-center justify-content-center">
 						<img class="mitad rounded-3" src="" alt="">
-						<form class="w-75" action="">
+						<audio style="display: none;" id="cancion" controls>
+							<source src="" type="audio/mpeg">
+						</audio>
+						<form class="mt-3 mitad" action="">
 							<input class="w-100" type="range" name="tiempo-reproduccion" id="tiempo-reproduccion">
+							<div class="d-flex justify-content-between">
+								<span class="text-white" id="tiempo-actual">00:00</span>
+								<span class="text-white" id="duracion">00:00</span>
+							</div>
 							<div class="d-flex justify-content-around" id="botones-reproducir">
 								<button class="btn text-white"><i class="fa-solid fa-shuffle"></i></button>
 								<button class="btn text-white"><i class="fa-solid fa-backward-step"></i></button>
-								<button class="btn text-white fs-1"><i class="fa-solid fa-circle-play"></i></button>
+								<button class="btn text-white fs-1 play"><i class="fa-solid fa-circle-play"></i></button>
 								<button class="btn text-white"><i class="fa-solid fa-forward-step"></i></button>
 								<button class="btn text-white"><i class="fa-solid fa-repeat"></i></button>
 							</div>
