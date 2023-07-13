bce66bfb2b4d397043da1f90f550cabd85694b54
Jul879n <l.araya@duocuc.cl>
2023-07-13 10:14:21 -0400

    Update mis-scripts.js

--- assets/librerias/js/mis-scripts.js
+++ assets/librerias/js/mis-scripts.js
@@ -245,12 +245,11 @@ jQuery(document).ready(function ($) {
   var repetir = false;
   var mix = false;
   var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
   $(".img-cancion").on("click", function () {
-  console.log("hola");
-  posicion = $(this).parent().parent().parent().index();
-  console.log(posicion);
-  cargarCancion();
- });
+    console.log("hola");
+    
+  });
   function cargarCancion() {
     var audioCancion = $('#contenido .song').eq(posicion).data('src');
     var infoCancion = $('#contenido .song').eq(posicion).data('info');
