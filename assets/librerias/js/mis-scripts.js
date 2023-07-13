2b7354667bbaab08be8f21dbd7a899216965a574
Jul879n <l.araya@duocuc.cl>
2023-07-13 10:08:33 -0400

    casi 2

--- assets/librerias/js/mis-scripts.js
+++ assets/librerias/js/mis-scripts.js
@@ -245,11 +245,12 @@ jQuery(document).ready(function ($) {
   var repetir = false;
   var mix = false;
   var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  $(".img-cancion").click(function() {
-    posicion = $(this).parent().parent().parent().index();
-    console.log(posicion);
-    cargarCancion();
-  });
+ $(".img-cancion").click(function () {
+  console.log("hola");
+  posicion = $(this).parent().parent().parent().index();
+  console.log(posicion);
+  cargarCancion();
+ });
   function cargarCancion() {
     var audioCancion = $('#contenido .song').eq(posicion).data('src');
     var infoCancion = $('#contenido .song').eq(posicion).data('info');
